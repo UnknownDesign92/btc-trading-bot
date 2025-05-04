5ba8e8fba9471ea39128a894dcd5e9be35b2ee7f
UnknownDesign92 <150618127+UnknownDesign92@users.noreply.github.com>
2025-05-04 12:14:31 +0200

    Delete discord_notifier.py

--- discord_notifier.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Funktion zum Senden einer Nachricht an Discord
-def send_discord_message(webhook_url, message):
-    """Sendet eine Nachricht an Discord über den Webhook."""
-    data = {"content": message}
-    response = requests.post(webhook_url, json=data)
-    if response.status_code != 204:
-        print(f"Fehler beim Senden der Nachricht: {response.status_code}")
